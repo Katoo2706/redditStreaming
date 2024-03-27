98e13f471f0bb47efcc0200a371661b1611ddc5f
Kato <katoo2706@gmail.com>
2024-03-27 15:59:10 +0700

    Add start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-docker build -t spark-base:latest ./docker/base
-docker build -t spark-master:latest ./docker/spark-master
-docker build -t spark-worker:latest ./docker/spark-worker
-docker build -t spark-submit:latest ./docker/spark-submit
+# Build Spark images
+docker build -t spark-base:latest ./spark/base
+docker build -t spark-master:latest ./spark/spark-master
+docker build -t spark-worker:latest ./spark/spark-worker
+docker build -t spark-submit:latest ./spark/spark-submit
 
-docker-compose up --scale spark-worker=3
+# Start Spark cluster
+docker-compose -f spark/docker-compose.yml up --scale spark-worker=3 -d
+
+# Start fastAPI and Kafka compose
+docker-compose -f kafka/docker-compose.yml up -d
